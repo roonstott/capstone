220cb98c47bc4e5f0e704d49517ef9c0c470faca
Robert Onstott <roonstott@gmail.com>
2023-03-05 13:46:04 -0800

    WIP: Still working on firebase db update functions

--- colab_design/src/components/DatabaseFunctions.js
+++ colab_design/src/components/DatabaseFunctions.js
@@ -6,18 +6,34 @@ const makeUser = async (uid, e, first, last) => { //called once on account creat
     firstName: first,
     lastName: last,
     email: e, 
-    collaborators: [], //array of id's
-    projectsOwned: [], //array of id's
-    projectsInvited: [], //array of id's
-    projectsJoined: [] //array of id's
+    collaborators: [], //array of id's to be updated as user adds collaborators
+    projectsOwned: [], //array of id's to be updated as user makes projects
+    projectsInvited: [], //array of id's to be updated as user is invited to projects
+    projectsJoined: [] //array of id's to be updated as user accepts project invitations
   }
-  await setDoc(doc(db, "users", uid)), profile;  
+  await setDoc(doc(db, "users", uid), profile);  
 }
 
-const makeProject = async () => {
+const makeProject = async (owner, title, description, invitations) => { //called each time a user makes a project
   const project = {
-
+    owner,
+    title,
+    description,
+    invitations, //array of uid's
+    collaborators: [], //empy array to be filled with uid's as invitees accept
+    editHistory: [] //empy array to be updated as people add edits
   }
+
+  await addDoc(collection(db, "projects"), project) // adds project to collection of all project
+  //still need to send invitations to invitees
+
+}
+
+const addCollaborator = async (projId, colabUid) => { //called each time an invitee accepts an invitation: updates project to have user, and updateds user to have project
+  const proj = await doc(db, "projects", projId );
+  const user = await doc(db, "users", colabUid);
+  const colabArray = proj.collaborators;
+  const updated
 }
 
 
